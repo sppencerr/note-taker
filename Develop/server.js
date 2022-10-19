f80a4e1bec2fd6c293c4168397e98455eacf84cf
Spencer Gerritsen <spencergerritsen@gmail.com>
2022-10-18 23:17:23 -0600

    functioning

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const app = express('');
-const PORT = process.env.PORT || 3000;
-const routes = require('');
+const app = express();
+const PORT = process.env.PORT || 3001;
+const routes = require('./routes/routes');
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.static('./public'));
+app.use(express.json());
+app.use('/', routes);
+
+app.listen(PORT, function() {
+    console.log(`Running on port ${PORT}`);
+});
+
+
